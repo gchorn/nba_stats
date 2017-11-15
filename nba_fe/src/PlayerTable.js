90f892403726cc63951a699c3040e0a04c154277
Guillaume Chorn <guillaume.chorn@zefr.com>
2017-11-14 22:56:34 -0800

    Add pagination, searching and styling to FE

--- nba_fe/src/PlayerTable.js
+++ nba_fe/src/PlayerTable.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import {Col, Input, Pagination, ProgressBar, Row} from 'react-materialize'
+import './PlayerTable.css';
+
+var _ = require('lodash')
+var pageSize = 100;
 
 class PlayerTable extends Component {
   constructor() {
     super();
+    this.changePage = this.changePage.bind(this);
+    this.playerSearchUpdate = this.playerSearchUpdate.bind(this);
+    this.playerSearchInputChanged = this.playerSearchInputChanged.bind(this);
     this.state = {
       players: [],
+      pages: 0,
+      page: 1,
+      loading: 'ProgressBar-hidden',
+      pagination: 'Pagination-hidden',
+      searchValue: null
     };
   }
 
-  componentWillMount() {
-    fetch('http://localhost/players?format=json')
+  fetchPlayers() {
+    this.setState({ loading: null });
+
+    let fetchUrl = 'http://localhost/players?format=json&page=' + this.state.page
+    if (this.state.searchValue != null) {
+      fetchUrl = 'http://localhost/players/search?format=json&name=' + this.state.searchValue
+    }
+
+    fetch(fetchUrl)
       .then(results => {
         return results.json();
       }).then(data => {
@@ -27,15 +47,54 @@ class PlayerTable extends Component {
             </tr>
           )
         })
+        let pageNum = _.ceil(data.count/pageSize);
+        if (this.state.pages !== pageNum) {
+          this.setState({ pages: pageNum });
+          if (pageNum === 0) {
+            console.log('hiding pagination!')
+            this.setState({ pagination: 'Pagination-hidden' });
+          } else {
+            console.log('showing pagination!')
+            this.setState({ pagination: null });
+          }
+        }
         this.setState({ players: players });
-        console.log("state", this.state.players);
+        this.setState({ loading: 'ProgressBar-hidden' });
       })
+  }
+
+  changePage(pageNumber) {
+    this.setState({ page: pageNumber });
+  }
+
+  playerSearchInputChanged(event, value) {
+    this.handleSearchDebounced(value);
+  }
+
+  playerSearchUpdate(value) {
+    this.setState({ searchValue: value });
+  }
+
+  componentWillMount() {
+    this.handleSearchDebounced = _.debounce(this.playerSearchUpdate, 350);
+    this.fetchPlayers();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.page !== prevState.page || this.state.searchValue !== prevState.searchValue) {
+      this.fetchPlayers();
+    }
   }
 
   render() {
     return (
       <div className="tableContainer">
-        <h1>NBA Players</h1>
+        <Row>
+          <Col s={7}><h1 className="pageTitle">NBA Players</h1></Col>
+          <Input placeholder="Search players by name" s={5} onChange={this.playerSearchInputChanged}/>
+        </Row>
+        <Pagination className={"tablePaginator "+ this.state.pagination} items={this.state.pages} activePage={this.state.page} maxButtons={8} onSelect={this.changePage}/>
+        <ProgressBar className={this.state.loading}/>
         <table className="playerTable highlight bordered">
           <thead>
             <tr>
